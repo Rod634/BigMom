28dbacbf36267305c6688eb6cb50d6c1bd536051
Rodrigo Silva <rodrigo.silva@take.net>
2022-05-02 14:33:59 -0300

    blip

--- BigMom/Driver/Blip.cs
+++ BigMom/Driver/Blip.cs
@@ -4,7 +4,7 @@ using OpenQA.Selenium.Support.UI;
 using System;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace BigMom.Driver
 {
@@ -15,7 +15,7 @@ namespace BigMom.Driver
         public Blip()
         {
             // init driver in the blip chat
-            _driver = new ChromeDriver();
+            _driver = new ChromeDriver($"C:/Users/Rodrigo.silva/Documents/bin");
             _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(120));
 
             Login();
@@ -25,7 +25,7 @@ namespace BigMom.Driver
         public void Login()
         {
             _driver.Navigate().GoToUrl(@"https://portal.blip.ai/");
-            _wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.Id("navbar")));
+            _wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.Id("main-navbar")));
         }
 
         public void LoadBotsAndUsers()
@@ -42,20 +42,26 @@ namespace BigMom.Driver
             users.ToList().ForEach(u => AddTeam(u));
         }
 
-        private async void AddTeam(string user)
+        private void AddTeam(string user)
         {
             _wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.XPath("/html/body/div[3]/div[1]/section/ui-view/section/div/page-header/div[1]/div[1]/div[1]/div[2]/custom-content/button")));
             //Click in add users
             IWebElement addButton = _driver.FindElement(By.XPath(@"/html/body/div[3]/div[1]/section/ui-view/section/div/page-header/div[1]/div[1]/div[1]/div[2]/custom-content/button"));
             _wait.Until(ExpectedConditions.ElementToBeClickable(addButton));
-            await Task.Delay(TimeSpan.FromSeconds(3));
+
             addButton.Click();
+
             //set user email
             _driver.FindElement(By.XPath(@"/html/body/div[7]/div[2]/div[2]/form/div[1]/material-input/div/input")).SendKeys(user);
             //add admin permission
             _driver.FindElement(By.XPath(@"/html/body/div[7]/div[2]/div[2]/form/div[1]/div[3]/ul/li[4]/span[2]")).Click();
             //save
-            _driver.FindElement(By.XPath(@"/html/body/div[7]/div[2]/div[2]/form/div[2]/button[2]")).Click();
+            IWebElement saveButton = _driver.FindElement(By.XPath(@"/html/body/div[7]/div[2]/div[2]/form/div[2]/button[2]"));
+            _wait.Until(ExpectedConditions.ElementToBeClickable(saveButton));
+
+            saveButton.Click();
+
+            Thread.Sleep(8000);
         }
 
     }
